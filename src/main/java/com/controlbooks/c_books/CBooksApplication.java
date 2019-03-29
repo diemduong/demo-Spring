3dc90ef17269bc480eb0e947b02b5d9365af9241
Diem Duong <diem.duong@tu-dortmund.de>
2019-03-29 07:42:47 +0100

    init commit

---
+++ src/main/java/com/controlbooks/c_books/CBooksApplication.java
@@ -0,0 +1,13 @@
+package com.controlbooks.c_books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CBooksApplication.class, args);
+    }
+
+}
